4591f47dacffc0f52471c009c6a1bb319888c814
oruxl <oruxl@outlook.com>
2018-05-26 15:42:15 -0400

    typo

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name='angrymetalpy',
       version='1.0',
       description='Python classes for representing Angry Metal Guy reviews and reviewers',
-      author='orxul',
-      url='https://www.github.com/orxul/angrymetalpy',
+      author='oruxl',
+      url='https://www.github.com/oruxl/angrymetalpy',
       packages=['angrymetalpy'],
       setup_requires=['numpy'],
      )
