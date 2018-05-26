b6f2f9fbe559bf6d5bb9c4b2f3520477a0f0e181
oruxl <oruxl@outlook.com>
2018-05-26 15:40:59 -0400

    setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='angrymetalpy',
+      version='1.0',
+      description='Python classes for representing Angry Metal Guy reviews and reviewers',
+      author='orxul',
+      url='https://www.github.com/orxul/angrymetalpy',
+      packages=['angrymetalpy'],
+      setup_requires=['numpy'],
+     )
+
